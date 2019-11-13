d63d69d371d32f11e26b8d2dde2c19725867c797
Will Freeman <hohosanta@me.com>
2019-11-12 21:22:19 -0600

    fix table head margin on mobile

--- src/components/TableHead.jsx
+++ src/components/TableHead.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Grid,
   ListItem,
@@ -6,54 +6,54 @@ import {
   Checkbox,
   Hidden
 } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/styles";
 
-const style = {
-  head: {
-    color: "#5d5d5d",
-    fontSize: "0.8em",
-    textAlign: "center",
-    paddingBottom: 0,
-    fontWeight: 600,
-    textTransform: "uppercase"
-  }
-};
+function TableHead(props) {
+  const theme = useTheme();
+  const small = useMediaQuery(theme.breakpoints.down("xs"));
 
-class TableHead extends Component {
-  render() {
-    return (
-      <ListItem divider style={style.head}>
-        <Hidden xsDown>
-          <Grid item sm={1}>
-            <Checkbox
-              checked={this.props.selected}
-              indeterminate={this.props.someSelected && !this.props.selected}
-              onChange={this.props.onCheckboxChange}
-            />
-          </Grid>
-          <Grid item sm={1}>
-            <IconButton
-              disabled={!this.props.someSelected}
-              onClick={this.props.onDelete}
-            >
-              <i className="material-icons">delete</i>
-            </IconButton>
-          </Grid>
-        </Hidden>
-        <Grid item xs={2}>
-          SPACE
+  const style = {
+    head: {
+      color: "#5d5d5d",
+      fontSize: "0.8em",
+      textAlign: "center",
+      paddingBottom: small ? 10 : 0,
+      fontWeight: 600,
+      textTransform: "uppercase"
+    }
+  };
+
+  return (
+    <ListItem divider style={style.head}>
+      <Hidden xsDown>
+        <Grid item sm={1}>
+          <Checkbox
+            checked={props.selected}
+            indeterminate={props.someSelected && !props.selected}
+            onChange={props.onCheckboxChange}
+          />
         </Grid>
-        <Grid item xs={4}>
-          RESERVED FOR
+        <Grid item sm={1}>
+          <IconButton disabled={!props.someSelected} onClick={props.onDelete}>
+            <i className="material-icons">delete</i>
+          </IconButton>
         </Grid>
-        <Grid item xs={3}>
-          FROM
-        </Grid>
-        <Grid item xs={3}>
-          TO
-        </Grid>
-      </ListItem>
-    );
-  }
+      </Hidden>
+      <Grid item xs={2}>
+        SPACE
+      </Grid>
+      <Grid item xs={4}>
+        RESERVED FOR
+      </Grid>
+      <Grid item xs={3}>
+        FROM
+      </Grid>
+      <Grid item xs={3}>
+        TO
+      </Grid>
+    </ListItem>
+  );
 }
 
 export default TableHead;
